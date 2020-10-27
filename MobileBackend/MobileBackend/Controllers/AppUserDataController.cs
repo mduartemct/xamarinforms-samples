d7d880c02fed07a48618f6827258bceecc0524b5
Marcelo Duarte <mduarte_mct@hotmail.com>
2020-10-27 09:19:26 -0300

    MobileBackEnd - Terminando a Implementação de Filter By UserSid para a TodoItem

---
+++ MobileBackend/MobileBackend/Controllers/AppUserDataController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using MobileBackend.DataObjects;
+using MobileBackend.Models;
+
+namespace MobileBackend.Controllers
+{
+    /*
+    The WebApiConfig class may require additional changes to add a route for this controller. Merge these statements into the Register method of the WebApiConfig class as applicable. Note that OData URLs are case sensitive.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using MobileBackend.DataObjects;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<AppUser>("AppUserData");
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class AppUserDataController : ODataController
+    {
+        private MobileServiceContext db = new MobileServiceContext();
+
+        // GET: odata/AppUserData
+        [EnableQuery]
+        public IQueryable<AppUser> GetAppUserData()
+        {
+            return db.AppUsers;
+        }
+
+        // GET: odata/AppUserData(5)
+        [EnableQuery]
+        public SingleResult<AppUser> GetAppUser([FromODataUri] string key)
+        {
+            return SingleResult.Create(db.AppUsers.Where(appUser => appUser.Id == key));
+        }
+
+        // PUT: odata/AppUserData(5)
+        public IHttpActionResult Put([FromODataUri] string key, Delta<AppUser> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            AppUser appUser = db.AppUsers.Find(key);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(appUser);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppUserExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(appUser);
+        }
+
+        // POST: odata/AppUserData
+        public IHttpActionResult Post(AppUser appUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.AppUsers.Add(appUser);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (AppUserExists(appUser.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Created(appUser);
+        }
+
+        // PATCH: odata/AppUserData(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] string key, Delta<AppUser> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            AppUser appUser = db.AppUsers.Find(key);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(appUser);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppUserExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(appUser);
+        }
+
+        // DELETE: odata/AppUserData(5)
+        public IHttpActionResult Delete([FromODataUri] string key)
+        {
+            AppUser appUser = db.AppUsers.Find(key);
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            db.AppUsers.Remove(appUser);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AppUserExists(string key)
+        {
+            return db.AppUsers.Count(e => e.Id == key) > 0;
+        }
+    }
+}
--- MobileBackend/MobileBackend/Controllers/TodoItemController.cs
+++ MobileBackend/MobileBackend/Controllers/TodoItemController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
@@ -19,7 +20,10 @@ namespace MobileBackend.Controllers
             DomainManager = new EntityDomainManager<TodoItem>(context, Request);
         }
 
-        public string SecurityClaim
+        /// <summary>
+        /// Utilizado para pegar o Secure Identifier do Usuário logado acessando a API.
+        /// </summary>
+        public string UserSid
         {
             get
             {
@@ -28,29 +32,45 @@ namespace MobileBackend.Controllers
             }
         }
 
+        /// <summary>
+        /// Utilizado para Validar um usuário autenticado nas operações de Insert, Update, Delete
+        /// </summary>
+        /// <param name="id"></param>
+        public void ValidateOwner(string id)
+        {
+            var result = Lookup(id).Queryable.PerUserFilter(UserSid).FirstOrDefault<TodoItem>();
+            if (result == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+        }
+
         // GET tables/TodoItem
         public IQueryable<TodoItem> GetAllTodoItems()
         {
-            return Query().Where(item => item.UserSId.Equals(SecurityClaim));
+            return Query().PerUserFilter(UserSid);
+            //return Query().Where(item => item.UserSId.Equals(UserSid));
             //return Query();
         }
 
         // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public SingleResult<TodoItem> GetTodoItem(string id)
         {
-            return Lookup(id);
+            return new SingleResult<TodoItem>(Lookup(id).Queryable.PerUserFilter(UserSid));
+            //return Lookup(id);
         }
 
         // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public Task<TodoItem> PatchTodoItem(string id, Delta<TodoItem> patch)
         {
+            ValidateOwner(id);
             return UpdateAsync(id, patch);
         }
 
         // POST tables/TodoItem
         public async Task<IHttpActionResult> PostTodoItem(TodoItem item)
         {
-            item.UserSId = SecurityClaim;
+            item.UserSId = UserSid;
             TodoItem current = await InsertAsync(item);
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
@@ -58,7 +78,16 @@ namespace MobileBackend.Controllers
         // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public Task DeleteTodoItem(string id)
         {
+            ValidateOwner(id);
             return DeleteAsync(id);
+        }
+    }
+
+    public static class TodoItemExtensions
+    {
+        public static IQueryable<TodoItem> PerUserFilter(this IQueryable<TodoItem> query, string userSid)
+        {
+            return query.Where(item => item.UserSId.Equals(userSid));
         }
     }
 }
